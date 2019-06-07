e488987b09e1158bc4962f8068329f29c2c3c13c
aucejito <tomanotademicorreo@hotmail.com>
2019-06-07 12:20:04 +0200

    Update mainFXMLController.java

--- src/intervaltimer/mainFXMLController.java
+++ src/intervaltimer/mainFXMLController.java
@@ -59,9 +59,9 @@ public class mainFXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
        
-        sesionComboBox.setPromptText("Seleccione 1º un grupo");
+        
         modGrupo.setDisable(true);
-        sesionComboBox.setDisable(true);
+       
         graphButton.setDisable(true);
         
         gruposArrayList = gimnasio.getGrupos();
@@ -84,10 +84,10 @@ public class mainFXMLController implements Initializable {
             if (grupoComboBox.getSelectionModel().getSelectedIndex() > -1 ) {
                 grupoActual = gruposArrayList.get(grupoComboBox.getSelectionModel().getSelectedIndex());
                 modGrupo.setDisable(false);
-                sesionComboBox.setDisable(false);
+                
                 graphButton.setDisable(false);
                 
-                sesionComboBox.setPromptText("Seleccione sesión");
+                
             }
 
         });
